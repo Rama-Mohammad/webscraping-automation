f988ccd4e76b6de78c4fc9e7cd9efc42448d00f2
Rama <rama_marwan@hotmail.com>
2026-02-28 14:06:04 +0200

    added no sandbox since the error is from selenium...

--- bitcoin_scraper.py
+++ bitcoin_scraper.py
@@ -13,6 +13,8 @@ from selenium.webdriver.support import expected_conditions as EC
 # Set up Selenium options
 options = Options()
 options.add_argument("--headless")  # Enable headless mode for GitHub Actions
+options.add_argument("--no-sandbox") 
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
 
